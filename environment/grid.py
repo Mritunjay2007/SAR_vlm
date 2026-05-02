d1fd7e5f6a7dabec452934a928418a36beee5815
Mritunjay Kumar <mritunjay9b.32966@gmail.com>
2026-05-02 13:25:46 +0530

    initial code base

---
+++ environment/grid.py
@@ -0,0 +1,21 @@
+import numpy as np
+import random
+
+class GridEnvironment:
+    """
+    Represents the ground truth world.
+    The drone does NOT know this.
+    """
+    def __init__(self, size):
+        self.size = size
+        self.grid = np.zeros((size, size))
+        self.victim_pos = self._place_victim()
+
+    def _place_victim(self):
+        x = random.randint(0, self.size - 1)
+        y = random.randint(0, self.size - 1)
+        self.grid[x][y] = 1
+        return (x, y)
+
+    def is_victim(self, pos):
+        return self.grid[pos[0]][pos[1]] == 1
